111b656dc8c010d276c259456c05556037963210
alexey.taran <alexey.taran@energyworx.org>
2023-04-14 19:28:26 +0200

    wip: hw6 user model

---
+++ hw6/js/userModel.js
@@ -0,0 +1,20 @@
+class User {
+    constructor(age, city, sport) {
+        this.age = age
+        this.city = city
+        this.sport = sport
+    }
+
+    
+
+}
+
+
+const birthDate = prompt("What is your date of Birth?")
+const theCityOfLiving = prompt("Where do you live?")
+const favouriteSport = prompt("What is your favourite sport?")
+
+
+alert(`Given hours of ${givenHours} gonna be ${hoursToSecondsConverter(givenHours)} in seconds`)
+
+
