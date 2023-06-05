35a1f9ae3f98843fa5b5dc511185603e8b56dadb
alexey.taran <alexey.taran@energyworx.org>
2023-06-05 12:39:14 +0200

    refactor: move createElements and dataStore to emarket.js

--- lessons/hw21/js/data.js
+++
@@ -1,8 +0,0 @@
-const dataStore = [
-	{ id: 1, title: "Renault", description: "Just a fun hot hatch", category: "cars" },
-	{ id: 2, title: "Tesla", description: "Economy car", category: "cars" },
-	{ id: 3, title: "Harley Devidson", description: "Extra comfortable bike", category: "motorcycles" },
-	{ id: 4, title: "Suzuki", description: "Really fast bike", category: "motorcycles" },
-	{ id: 5, title: "Giant", description: "Awesome gravel bike", category: "bikes" },
-	{ id: 6, title: "Trek", description: "Fast road bike", category: "bikes" },
-]
